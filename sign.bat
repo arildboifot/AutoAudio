18b551889cf76d7c8bb934f2920ff2fab3ead644
arild.boifot <arild.boifot@gmail.com>
2013-11-27 21:01:03 +0100

    Better deploy

---
+++ sign.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set env=%1
+set pfx=%2
+set pwd=%3
+set v=%4
+
+set bin=%~dp0AutoAudio\bin\%env%
+set rootDeploy=%~dp0deploy
+set deploy=%rootDeploy%\%v%
+
+md %deploy%
+xcopy %bin%\*.* %deploy% /S /Y
+
+for /R "%deploy%" %%i in (*exe*) do signtool sign /v /d AutoAudio /f %pfx% /p %pwd% /tr "http://www.startssl.com/timestamp" "%%i"
+for /R "%deploy%" %%i in (*dll*) do signtool sign /v /d AutoAudio /f %pfx% /p %pwd% /tr "http://www.startssl.com/timestamp" "%%i"
+
+mage -New Application -Processor x86 -ToFile %deploy%\AutoAudio.exe.manifest -Name "AutoAudio" -Version %v% -FromDirectory %deploy%
+mage -sign %deploy%\AutoAudio.exe.manifest -CertFile %pfx% -Password %pwd%
+mage -New Deployment -Processor x86 -Install true -Publisher "CottonCactus" -ProviderUrl "http://www.boifotsoftware.no/apps/AutoAudio/AutoAudio.application" -AppManifest %deploy%\AutoAudio.exe.manifest -ToFile %rootDeploy%\AutoAudio.application
+mage -sign %rootDeploy%\AutoAudio.application -CertFile %pfx% -Password %pwd%
